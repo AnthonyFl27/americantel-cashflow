dcd49d414a7d76f51c2e2b347e57529181be42c5
AleZAS1406 <zarruksancheza33@gmail.com>
2025-11-13 17:40:46 -0600

    Ventas

---
+++ src/main/java/org/example/americantelcashflow/modelo/Venta.java
@@ -0,0 +1,35 @@
+package org.example.americantelcashflow.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "venta")
+@Getter
+@Setter
+public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Compatible con PostgreSQL SERIAL
+    @Column(name = "id_venta")
+    private Long idVenta;
+
+    @Column(name = "fecha_hora", nullable = false)
+    private LocalDateTime fechaHora = LocalDateTime.now();
+
+    @Required
+    @Column(nullable = false)
+    private double total;
+
+    @Column(length = 200)
+    private String descripcion;
+
+    // ---------------- RELACIÓN CON CAJA ----------------
+    @ManyToOne
+    @JoinColumn(name = "id_caja") // FK en PostgreSQL
+    private Caja caja;
+}
