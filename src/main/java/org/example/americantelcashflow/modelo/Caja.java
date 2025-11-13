304f390bcba38808013354b9ac92d7779df08833
rodzzpr <rprador@uamv.edu.ni>
2025-11-13 17:28:54 -0600

    Entidad Caja implementada

---
+++ src/main/java/org/example/americantelcashflow/modelo/Caja.java
@@ -0,0 +1,89 @@
+package org.example.americantelcashflow.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Caja {
+
+    private Long idCaja;
+    private LocalDateTime fechaApertura;
+    private LocalDateTime fechaCierre;
+    private double montoInicial;
+    private double montoFinal;
+    private double saldoActual;
+
+    private List<Venta> ventas = new ArrayList<>();
+    private List<MovimientoCaja> movimientos = new ArrayList<>();
+
+    // ------------------- GETTERS & SETTERS -------------------
+
+    public Long getIdCaja() {
+        return idCaja;
+    }
+
+    public void setIdCaja(Long idCaja) {
+        this.idCaja = idCaja;
+    }
+
+    public LocalDateTime getFechaApertura() {
+        return fechaApertura;
+    }
+
+    public void setFechaApertura(LocalDateTime fechaApertura) {
+        this.fechaApertura = fechaApertura;
+    }
+
+    public LocalDateTime getFechaCierre() {
+        return fechaCierre;
+    }
+
+    public void setFechaCierre(LocalDateTime fechaCierre) {
+        this.fechaCierre = fechaCierre;
+    }
+
+    public double getMontoInicial() {
+        return montoInicial;
+    }
+
+    public void setMontoInicial(double montoInicial) {
+        this.montoInicial = montoInicial;
+    }
+
+    public double getMontoFinal() {
+        return montoFinal;
+    }
+
+    public void setMontoFinal(double montoFinal) {
+        this.montoFinal = montoFinal;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    public List<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public List<MovimientoCaja> getMovimientos() {
+        return movimientos;
+    }
+
+    public void setMovimientos(List<MovimientoCaja> movimientos) {
+        this.movimientos = movimientos;
+    }
+}
